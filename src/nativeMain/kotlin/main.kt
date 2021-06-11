57b504765e5bf0a95f7a9329a72b4b68f82b3aa9
Joakim Forslund <joakim.forslund@gloot.com>
2021-06-11 07:39:02 +0200

    Cleanup

--- src/nativeMain/kotlin/main.kt
+++ src/nativeMain/kotlin/main.kt
@@ -2,9 +2,9 @@ import keystone.*
 import kotlinx.cinterop.*
 
 fun main(){
-    val asm = "INC ecx; DEC edx"
     memScoped {
-        var data:ByteArray = byteArrayOf()
+        val asm = "INC ecx; DEC edx"
+        var data:ByteArray? = byteArrayOf()
         val ks:COpaquePointerVar = alloc()
         val count:COpaquePointerVar = alloc()
         val encode:COpaquePointerVar = alloc()
@@ -13,7 +13,7 @@ fun main(){
         println("Is ARM64 supported: ${ks_arch_supported(KS_ARCH_ARM64)}")
         println("Is x86 supported: ${ks_arch_supported(KS_ARCH_X86)}")
 
-        val err:ks_err = ks_open(KS_ARCH_X86, KS_MODE_32.toInt() + KS_MODE_LITTLE_ENDIAN.toInt(), ks.ptr.reinterpret())
+        val err:ks_err = ks_open(KS_ARCH_X86, KS_MODE_32.toInt(), ks.ptr.reinterpret())
         if(err != KS_ERR_OK){
             println("ERROR: failed on ks_open(), quit")
             return
@@ -26,12 +26,11 @@ fun main(){
             if(paddedSize == 0){
                 return
             }
-            data = encode.ptr.readBytes(paddedSize).copyOf()
-            println("Size of data: ${data.size} size: $paddedSize")
-            println("$asm = ${data.toHexString()}")
+            data = encode.ptr.pointed.value?.readBytes(paddedSize)
+            println("Size of data: ${data?.size} size: $paddedSize")
+            println("$asm = ${data?.toHexString()}")
             println("Compiled: ${size.value.toLong()} bytes, statements: ${count.value.toLong()}")
         }
-        println("We all done")
         ks_free(encode.value?.reinterpret())
         ks_close(ks.value?.reinterpret())
         return
